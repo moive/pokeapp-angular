1d642fb645fe0b44b3f8e8fe04d2b1722d1ddcfb
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-03 10:53:59 -0500

    working with the component navbar html

--- src/app/poke/poke.module.ts
+++ src/app/poke/poke.module.ts
@@ -4,9 +4,11 @@ import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [GridComponent, NavbarComponent, CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   exports: [GridComponent],
 })
 export class PokeModule {}

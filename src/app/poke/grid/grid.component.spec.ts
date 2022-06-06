4d5cc624c39b9adf1ffe8555113e70a9874200eb
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-05 21:06:09 -0500

    fix test grid component

--- src/app/poke/grid/grid.component.spec.ts
+++ src/app/poke/grid/grid.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridComponent } from './grid.component';
@@ -8,9 +9,9 @@ describe('GridComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GridComponent ]
-    })
-    .compileComponents();
+      declarations: [GridComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
